65ebec8fdf444b2fb69c2788fec49b9bc6b0a0e5
Dzmitry Yatsyna <dyfromby@gmail.com>
2020-03-14 16:49:51 +0300

    feat: add check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let dict_brackets = {};
+  let stack = [];
+  let arr = str.split("");
+  bracketsConfig.forEach(x => dict_brackets[x[0]]=x[1]);
+  for (let i of arr) {
+    if (i in dict_brackets && i != dict_brackets[i]) {
+      stack.push(i);
+    }
+    else if (i == dict_brackets[i]) {
+      if (i == stack[stack.length-1] && stack.length != 0) {
+        stack.pop();
+      }
+      else {
+        stack.push(i);  
+      }
+    }
+    else {
+      if (dict_brackets[stack[stack.length-1]] == i) {
+        stack.pop();
+      }
+      else {
+        return false; 
+      };
+    };
+  };
+  return stack.length ? false : true;
+};
